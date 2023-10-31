c0b3b6e1386a85b2125ed07b19b64995f4811ebc
amancooks08 <amandeepsingh.chhabra08@gmail.com>
2023-11-01 00:10:42 +0530

    feat: add command line interface for the debtGuardian

---
+++ debt_guardian/system.go
@@ -0,0 +1,55 @@
+package debt_guardian
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func System() (exit bool) {
+	financialSystem := NewFinancialSystem()
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Println("Enter a command (add_transaction, query_debt, query_money_owed, query_most_money_owed, query_most_debt, exit):")
+		scanner.Scan()
+		command := scanner.Text()
+
+		if command == "add_transaction" {
+			fmt.Println("Enter transaction (A,B,X):")
+			scanner.Scan()
+			transaction := scanner.Text()
+			parts := strings.Split(transaction, ",")
+			if len(parts) == 3 {
+				personA := parts[0]
+				personB := parts[1]
+				amount := 0
+				_, err := fmt.Sscanf(parts[2], "%d", &amount)
+				if err == nil {
+					financialSystem.addTransaction(personA, personB, amount)
+				}
+			}
+		} else if command == "query_debt" {
+			fmt.Println("Enter person:")
+			scanner.Scan()
+			person := scanner.Text()
+			debt := financialSystem.queryDebt(person)
+			fmt.Printf("%s owes %d\n", person, debt)
+		} else if command == "query_money_owed" {
+			fmt.Println("Enter person:")
+			scanner.Scan()
+			person := scanner.Text()
+			moneyOwed := financialSystem.queryMoneyOwed(person)
+			fmt.Printf("%s is owed %d\n", person, moneyOwed)
+		} else if command == "query_most_money_owed" {
+			person := financialSystem.queryMostMoneyOwed()
+			fmt.Printf("%s is owed the most money.\n", person)
+		} else if command == "query_most_debt" {
+			person := financialSystem.queryMostDebt()
+			fmt.Printf("%s owes the most money.\n", person)
+		} else if command == "exit" {
+			return true
+		}
+	}
+}
--- main.go
+++ main.go
@@ -1,3 +1,14 @@
 package main
 
-import ()
+import(
+	system "financial_system/debt_guardian"
+)
+
+func main() {
+	for {
+		exit := system.System()
+		if exit {
+			break
+		}
+	}
+}
